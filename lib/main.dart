7ff29c598ef0cbfa9958fd567795d7b86e335de9
Safietou2003 <167894923+Safietou2003@users.noreply.github.com>
2025-05-30 21:31:01 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  Future<bool> _loadSplash() async {
+    await Future.delayed(const Duration(seconds: 3)); 
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,11 +20,34 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: '/test',
-      routes: {
-        '/test': (context) => const ISMLoginApp(),
-      },
       debugShowCheckedModeBanner: false,
+      home: FutureBuilder<bool>(
+        future: _loadSplash(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashContent();
+          } else {
+            return const ISMLoginApp(); 
+          }
+        },
+      ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF462A1D),
+      body: Center(
+        child: Image.asset(
+          'assets/logo.png', 
+          width: 150,
+        ),
+      ),
     );
   }
 }
